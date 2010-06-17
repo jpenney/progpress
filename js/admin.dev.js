edcd6f8bf5ca6add52a95e5842b50b4c41f2c084
Jason Penney <jpenney@jczorkmid.net>
2010-06-17 22:15:06 +0000

    tagging version 0.8.6

---
+++ js/admin.dev.js
@@ -0,0 +1,45 @@
+// $Id$
+// ProgPress
+jQuery(function($) {
+
+    function toggleStyles($link) {
+        var cell =  $($link.parents('tr')[0]).find('td'),
+            row2 =  $('tr#jcp_progpress_sample_output');
+        if ($link.text().indexOf('View') === 0) {
+            $link.text($link.text().replace(/^View +/,''));
+            cell.fadeIn("slow");
+            row2.fadeIn("slow");
+        } else {
+            $link.text('View ' + $link.text());
+            cell.fadeOut("slow");
+            row2.fadeOut("slow");
+        }
+    }
+
+    
+    var loadStyles = function() {       
+        var $this = $(this), 
+            href, previewRow, previewCell;
+        if ($this.data("loaded")) {
+            toggleStyles($this);
+        } else {
+            href = $this.attr('href');
+            $.get(href, function(data) {
+                previewRow = $($this.parents('tr')[0]);
+                previewCell =  $('td', previewRow);
+                previewCell.append($('<span>Please wait...</span>'));
+                previewCell.fadeOut("slow", function() {
+                    previewCell.html(
+                        $('<pre style="overflow:auto;" />').text(data));
+                    $this.attr('href','#').attr('target','');
+                    $this.data("loaded",true);
+                    toggleStyles($this);
+                });
+                $('head').append($('<style type="text/css"/>').text(data));
+            });
+        }
+        return false;
+    };
+
+    $('#jcp_progpress_preview_styles').bind('click',loadStyles);
+});
