990fc2abe3c44e2a68de21ec0db9c493da8a426b
Jason Penney <jpenney@jczorkmid.net>
2011-11-10 22:48:25 +0000

    updated prepare.sh to use closure-compiler, regenerated production styles and javascript

--- prepare.sh
+++ prepare.sh
@@ -30,17 +30,38 @@ if ! have yuicompressor; then
     exit 1
 fi
 
+HAVE_CLOSURE=0
+if have closure-compiler; then
+	HAVE_CLOSURE=1
+else
+	echo "unable to find closure-compliler, using yuicompressor for javascript"
+fi
+
 # are we using BSD or GNU stat command?
 STAT_FMT="-c%s"
 stat --help >/dev/null 2>&1 || STAT_FMT="-f%z"
 
+HEADER="/*Copyright $(date +'%Y') Jason Penney*/"
 
-for js in $(find . -name '*.dev.js'); do
+
+JQ_EXT="third-party/closure-externs/jquery-1.6.js"
+
+for js in $(find . -name '*.dev.js' | grep -v '\#'); do
     # use jsl if available
     have jsl && jsl -nologo -nofilelisting -nosummary  -process "$js"
     DEST="$(dirname $js)/$(basename $js .dev.js).js"
     echo "----------------- $js --------------------"
-    yuicompressor --type js "$js" > "$DEST"
+    printf "$HEADER" > "$DEST" 
+    if [[ $HAVE_CLOSURE == 0 ]]; then
+	yuicompressor --type js "$js" >> "$DEST"
+    else
+        if [[ -f "$JQ_EXT" ]]; then
+            closure-compiler --warning_level VERBOSE --externs "$JQ_EXT" \
+                --js "$js" >> "$DEST"
+        else
+	    closure-compiler --js "$js" >> "$DEST"
+        fi
+    fi
     SAV=$(echo "100 - ((100*$(stat $STAT_FMT $DEST))/$(stat $STAT_FMT $js))"|bc)
     echo " - compressed $SAV%"
 done
@@ -48,7 +69,8 @@ done
 for css in $(find . -name '*.dev.css'); do
     DEST="$(dirname $css)/$(basename $css .dev.css).css"
     echo "----------------- $css --------------------"
-    yuicompressor --type css "$css" > "$DEST"
+    printf "$HEADER" > "$DEST" 
+    yuicompressor --type css "$css" >> "$DEST"
     SAV=$(echo "100 - ((100*$(stat $STAT_FMT $DEST))/$(stat $STAT_FMT $css))"|bc)
     echo " - compressed $SAV%"
 done
